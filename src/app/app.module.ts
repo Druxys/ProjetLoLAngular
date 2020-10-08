4e5e7dfa6c7e42b4e774dd85f74705dabc66d405
Druxys <drudrux@gmail.com>
2020-10-08 10:11:23 +0200

    creating a route and generate a page for the profil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Pages/home/home.component';
+import { ProfilUserComponent } from './Pages/profil-user/profil-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProfilUserComponent
   ],
   imports: [
     BrowserModule
