e8cc3b4ed03799ae90f60adb174fc3e4756067c2
Druxys <drudrux@gmail.com>
2020-10-08 10:28:31 +0200

    adding route for home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {ProfilUserComponent} from './Pages/profil-user/profil-user.component';
+import {HomeComponent} from './Pages/home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'profil', component: ProfilUserComponent }
 ];
 
