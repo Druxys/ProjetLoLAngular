4d055ba2b2fd716cfbae3a038b01d84e6a656998
Druxys <drudrux@gmail.com>
2020-11-18 09:31:33 +0100

    improving the ârsing of the JSON

--- src/app/Pages/details-match/details-match.component.ts
+++ src/app/Pages/details-match/details-match.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ChampionService} from '../../Services/champion.service';
-
+// @ts-ignore
+import matchJSON from '../../../assets/json/details-match.json'
 @Component({
   selector: 'app-details-match',
   templateUrl: './details-match.component.html',
@@ -9,29 +10,67 @@ import {ChampionService} from '../../Services/champion.service';
 export class DetailsMatchComponent implements OnInit {
 
   champions;
+  matchArrayParticipant;
+  matchArrayParticipantDetails;
   champion;
   championArray;
   version;
-  key = '51';
+  participantId;
+  key;
+  summonerName = 'Phyrro';
   i = 0;
+  splash;
+  gameVersion: string;
+  stats: any;
   constructor(private championService: ChampionService) {
   }
 
   ngOnInit(): void {
+    console.log(matchJSON);
+    this.matchArrayParticipant = Object.values(matchJSON.participantIdentities);
+    this.matchArrayParticipantDetails = Object.values(matchJSON.participants);
+    this.gameVersion = matchJSON.gameVersion;
+    console.log(this.matchArrayParticipantDetails);
+
+
+    // GET THE PARTICIPANT ID
+    this.matchArrayParticipant.forEach(participant => {
+      if (participant.player.summonerName === this.summonerName){
+        this.participantId = participant.participantId;
+      }
+    });
+    console.log(this.participantId);
+
+    // GET THE CHAMPION ID OF THE PARTICIPANT
+    this.matchArrayParticipantDetails.forEach(participant => {
+      if (participant.participantId === this.participantId){
+        this.stats = participant.stats;
+        this.key = participant.championId.toString();
+      }
+    });
+    console.log(this.key);
+    console.log(this.stats);
+
+
+
+
+
+    // GET CHAMPION OF THE SELECTED PLAYER
     this.championService.getChampionAll().subscribe(res => {
       this.champions = res;
 
       console.log(this.champions.data);
       const length = Object.keys(this.champions.data).length;
       this.championArray = Object.values(this.champions.data);
-      console.log(this.championArray);
+      // console.log(this.championArray);
 
       this.championArray.forEach(value => {
           if (value.key === this.key) {
             this.champion = value;
-            console.log(this.champion);
+            // console.log(this.champion);
           }
       });
+      this.splash = this.championService.splashChamp + this.champion.name +'_0.jpg';
       this.championService.getSpecificChampion(this.champion.name).subscribe(res => {
         console.log(res.data);
       });
--- src/app/Services/champion.service.ts
+++ src/app/Services/champion.service.ts
@@ -11,13 +11,13 @@ import {VersionService} from './version.service';
 })
 export class ChampionService {
 
-
+  public version;
   champList = 'https://ddragon.leagueoflegends.com/cdn/10.22.1/data/fr_FR/champion.json';
 
   specificChamp = 'https://ddragon.leagueoflegends.com/cdn/10.22.1/data/fr_FR/champion/';
 
   // Splash Art du champion du joueur
-  splashChamp = 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg';
+  splashChamp = 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/';
 
   // Icone du champ du joueur
   squareChamp = 'http://ddragon.leagueoflegends.com/cdn/10.22.1/img/champion/Aatrox.png';
@@ -28,25 +28,15 @@ export class ChampionService {
   public versionChamp: any;
 
   constructor(private http: HttpClient, public versionService: VersionService) {
-    // this.getVersion().then(r => {
-    //   this.versionChamp = r;
-    // });
+
   }
 
   getVersion() {
-    // return new Promise(function(resolve, reject)
-    // {
-    //   this.versionService.getVersionDataDragon().subscribe((data : any) => {
-    //     console.log('VersionDD :' + data.n.champion);
-    //     // this.versionChamp = data.n.champion;
-    //     resolve(data.n.champion)
-    //   });
-    // }) ;
+
   }
 
   getChampionAll(): Observable<Champion> {
-    // const version = await this.getVersion();
-    // const url = `https://ddragon.leagueoflegends.com/cdn/${version}/data/fr_FR/champion.json`;
+    // const url = `https://ddragon.leagueoflegends.com/cdn/${this.version.champion}/data/fr_FR/champion.json`;
     const url = `https://ddragon.leagueoflegends.com/cdn/10.22.1/data/fr_FR/champion.json`;
     return this.http.get<Champion>(url).pipe(
       tap(_ => console.log('get version')),
